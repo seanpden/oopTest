859f00cadfeea42be0de607337e2f5c5ee0ac64b
Sean Denney <seanden522@gmail.com>
2022-05-16 19:22:33 -0500

    comment cleanup

--- driver.py
+++ driver.py
@@ -4,8 +4,19 @@ import datainterface
 import pandas as pd
 # ------------------------------------------------------------
 
-di = datainterface.DataInterface() # init object
-di.setdata(pd.read_csv("AMLdata.csv")) # sets data
+# init datainterface object
+di = datainterface.DataInterface()
+
+# sets 'AMLdata.scv' as the dataset for DataInterface object
+di.setdata(pd.read_csv("AMLdata.csv"))
+
+# split data into features and labels by preset split function
+# specific to THIS DATASET'S CYTOMETRY DATA
 features, labels = di.split_by_cyto() # stores features and labels
-z = di.gauss_naive_bayes(features, labels, randomstate=51) # perform gauss naive bayes algo with a randomstate seed of 51
+
+# performs gaussian naive bayes function on features and labels with seed of 51,
+# stores result
+z = di.gauss_naive_bayes(features, labels, randomstate=51)
+
+# prints gaussian navie bayes
 print(z)
